d82b75b73fc8d1e46ee8570c0b4e2e2674eb286c
YuqiChou <zhouyuqi.master@gmail.com>
2012-09-05 18:20:10 +0800

    show debug sql in console

--- HelloDjango/settings.py
+++ HelloDjango/settings.py
@@ -158,13 +158,22 @@ LOGGING = {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+        },
     },
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
+        },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level':'DEBUG',
         },
     }
 }
